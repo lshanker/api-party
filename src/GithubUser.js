e09677b1c2fc479ac9605cef42f914cb7143c5d3
Seth Baughman <sbaughman1@me.com>
2017-06-20 13:37:34 -0400

    Add Nasa and Homework routes and components

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './GithubUser.css'
 
 class GithubUser extends Component {
   state = {
@@ -11,16 +12,23 @@ class GithubUser extends Component {
       html_url: ''
     }
   }
-
+  
   constructor(props) {
     super(props)
-    this.fetchUserData()
+    this.fetchUserData(props)
   }
 
-  fetchUserData = () => {
-    fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+  fetchUserData(props) {
+    fetch(`https://api.github.com/users/${props.match.params.username}`)
       .then(response => response.json())
       .then(user => this.setState({ user }))
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const locationChanged = (nextProps.location !== this.props.location)
+    if (locationChanged) {
+      this.fetchUserData(nextProps)
+    }
   }
 
   render() {
@@ -29,9 +37,9 @@ class GithubUser extends Component {
       <div className="github-user">
         <img src={user.avatar_url} alt="user"/>
         <h2>{user.login}</h2>
-        <h3>Followers: {user.followers}</h3>
-        <h3>Following: {user.following}</h3>
-        <h3>Location: {user.location}</h3>
+        <h3>followers: {user.followers}</h3>
+        <h3>following: {user.following}</h3>
+        <h3>location: {user.location}</h3>
         <a href={user.html_url} target="_">Link to {user.login}'s profile</a>
       </div>
     )
